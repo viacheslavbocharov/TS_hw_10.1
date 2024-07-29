f61fe06036a373f54ad570d7ed81edc9b6fff98b
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2024-07-29 10:11:31 +0100

    fixed code

--- hw_10.1.ts
+++ hw_10.1.ts
@@ -1,28 +1,23 @@
 // Вам потрібно створити тип DeepReadonly який буде робити доступними тільки для читання навіть властивості вкладених обʼєктів.
 type DeepReadonly<T> = {
-    readonly [K in keyof T]: T[K] extends object ? DeepReadonly<T[K]> : T[K];
-};
+    readonly [K in keyof T]: DeepReadonly<T[K]>;
+  };
 
 // Вам потрібно створити тип DeepRequireReadonly який буде робити доступними тільки для читання навіть властивості вкладених обʼєктів та ще й робити їх обовʼязковими.
 type DeepRequireReadonly<T> = {
-    readonly [K in keyof T]-?: T[K] extends object ? DeepRequireReadonly<T[K]> : T[K];
-};
+    readonly [K in keyof T]-?: DeepRequireReadonly<T[K]>;
+  };
 
 
 // Вам потрібно створити тип UpperCaseKeys, який буде приводити всі ключі до верхнього регістру. 
 type UpperCaseKeys<T> = {
     [K in keyof T as Uppercase<K & string>]: T[K];
-};
+  };
 
 //   Створіть тип ObjectToPropertyDescriptor, який перетворює звичайний обʼєкт на обʼєкт де кожне value є дескриптором.
-type CustomPropertyDescriptor<T> = {
-    value: T;
-    writable?: boolean;
-    enumerable?: boolean;
-    configurable?: boolean;
+type ObjectToPropertyDescriptor<T> = {
+    [K in keyof T]: TypedPropertyDescriptor<T[K]>;
   };
   
-  type ObjectToPropertyDescriptor<T> = {
-    [K in keyof T]: CustomPropertyDescriptor<T[K]>;
-  };
+  
 
